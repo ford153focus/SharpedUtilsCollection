b7a399571f9f222f36bda245b3e6d4303e52feae
Joe Ford <ford153focus@gmail.com>
2023-05-19 07:56:20 +0300

    new utils

---
+++ Paths.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+namespace SharpedUtilsCollection;
+
+public class Paths
+{
+    static string GetTemporaryDirectory()
+    {
+        var folder = Path.Combine(
+            Path.GetTempPath(),
+            Path.GetFileNameWithoutExtension(
+                Path.GetRandomFileName()
+            )
+        );
+
+        if (!Directory.Exists(folder)) Directory.CreateDirectory(folder);
+
+        return folder;
+    }
+}
---
+++ ProcUtils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace SharpedUtilsCollection;
+
+public class ProcUtils
+{
+    static void exec(string cmd)
+    {
+        string[] segments = cmd.Split(' ');
+
+        ProcessStartInfo startInfo = new ProcessStartInfo
+        {
+            FileName = segments[0],
+            Arguments = String.Join(' ', segments[1..]),
+            UseShellExecute = true,
+            CreateNoWindow = true,
+        };
+
+        var process = new Process();
+        process.StartInfo = startInfo;
+        process.Start();
+        process.WaitForExit();
+    }
+}
