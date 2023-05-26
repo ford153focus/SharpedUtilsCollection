57506e052e46e24618e2340791ab1c5d83e415d0
Joe Ford <ford153focus@gmail.com>
2023-05-26 07:02:55 +0300

    +1 pptr ext

--- PuppeteerUtils/Extensions.cs
+++ PuppeteerUtils/Extensions.cs
@@ -1,15 +1,23 @@
+// ReSharper disable UnusedType.Global
+// ReSharper disable UnusedMember.Global
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable FieldCanBeMadeReadOnly.Global
 using System.Threading.Tasks;
 using PuppeteerSharp;
 
-namespace SharpedUtilsCollection.PuppeteerUtils
+namespace SharpedUtilsCollection.PuppeteerUtils;
+
+public static class Extensions
 {
-    public static class Extensions
+    public static async Task<string> GetInnerText(this IElementHandle el)
     {
-        public static async Task<string> GetInnerText(this IElementHandle el)
-        {
-            IJSHandle? property = await el.GetPropertyAsync("innerText");
-            string? value = await property.JsonValueAsync<string>();
-            return value ?? "";
-        }
+        return await GetPropertyValue(el, "innerText");
+    }
+
+    public static async Task<string> GetPropertyValue(this IElementHandle el, string propertyName)
+    {
+        IJSHandle? property = await el.GetPropertyAsync(propertyName);
+        string? value = await property.JsonValueAsync<string>();
+        return value ?? "";
     }
 }
